9f071866a95928426bb543db508daa4178bf97ba
MarioKernell <mario.valenzuela01@estudiant.upf.edu>
2021-01-27 16:03:37 +0000

    No hicimos commit de esto

--- Lab1/code/main.c
+++ Lab1/code/main.c
@@ -4,18 +4,48 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  // Ejercicio 1
-  printf("Ejercicio 1");
-  char* escaneo;
-  sscanf(escaneo, "%s", argv[1]);
-  printf("%d", escaneo);
+    // Ejercicio 1
 
+    int escaneo; 
 
-  // Ejercicio 2
-  int* array = malloc(50*sizeof(int));
+    sscanf(argv[1], "%d", &escaneo);
+    printf("%d\n", escaneo);
 
-  for(int i=0; i<50; i++){
-    array[i] = rand() 100%;
+    // Ejercicio 2
+    int* array = malloc(escaneo*sizeof(int));
 
-  }
+    for(int i=0; i<escaneo; i++){
+        array[i] = rand() % 100;
+        //printf("Numero %d: %d\n",i,array[i]);
+    }
+    
+    // Ejercicio 3/4/5/6
+    int file_data = open("nums.dat", O_CREAT | O_RDWR, 0644);
+    int file_txt = open("nums.txt", O_CREAT | O_RDWR, 0644);
+    int count = 0;
+
+    write(file_data, array, escaneo*3);
+
+    char* string = malloc(escaneo*sizeof(char)*sizeof(int));
+    for(int i=0; i<escaneo; i++){
+        char str[3];
+        sprintf(str, "%d", array[i]);
+        strcat(string, str);
+        strcat(string, "\n");
+    }
+    
+    write(file_txt, string, escaneo*3);
+    free(string);
+    free(array);
+    
+    // Ejercicio 7/8
+    
+    int* dat_array = malloc(escaneo*sizeof(int));
+    read(file_txt, dat_array, escaneo*sizeof(int));
+    printf("%ls", dat_array);
+
+    close(file_data);
+    close(file_txt);
+    
+    return 0;
 }
--- main.c
+++ main.c
@@ -2,7 +2,10 @@
 #include <unistd.h>  
 #include <string.h>
 #include <stdlib.h>
-
+  
 int main(int argc , char *argv [] ){
-    
+    int nums[10];
+char buff[10];
+
+int fd = open(“nums.dat”, O_CREAT | O_RDWR, 0644);
 }
