ef60255d4f215eff3916cc5f6f9ca4fe4cfc05cc
alexgimenez02 <alex.gimenez02@estudiant.upf.edu>
2021-01-28 19:40:00 +0000

    Apartados 4.1 hasta 4.3 hechos

---
+++ Lab2/code/shell.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "myutils.c"  
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct cmd_struct{
+    char cmd[80];
+    char args[10][80];
+    int nargs;
+}cmd_type;
+
+int main(int argc, char *argv[]){
+    //char* cmds;
+    int j = 0;
+    char ch;
+    //printf("- Ejercicio 4.1\n");
+    /*
+    int fd = open("cmds.txt", O_CREAT | O_RDWR, 0644);
+    while(read_split(fd, cmds, 10, &ch) > 0){
+        printf("%d) %s\n", ++j, cmds);
+    }  
+    close(fd);
+    */
+
+    printf("- Ejercicio 4.2 & 4.3\n");
+    int ncmds = 0;
+    cmd_type cmds[100];
+    for(int i = 0; i < 100; i++){
+        cmds[i].nargs = 0;
+    }
+    char ch2;
+    int fd2 = open("cmds_args.txt", O_CREAT | O_RDWR, 0644);
+    int res = read_split(fd2, cmds[ncmds].cmd, 80, &ch);
+    while(res > 0){
+        if(ch == ' '){
+            res = read_split(fd2, cmds[ncmds].args[cmds[ncmds].nargs], 80, &ch);
+        }else if(ch == '\n'){
+            printf("%d) cmd: %s %s\n",++j,cmds[ncmds].cmd, cmds[ncmds].args[cmds[ncmds].nargs]);
+            ncmds++;
+            res = read_split(fd2, cmds[ncmds].cmd, 80, &ch);   
+        }
+    }
+    close(fd2);
+
+    
+}
