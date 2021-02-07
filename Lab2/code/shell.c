d34050a0013ee941128c5fdcc3cdf43e30c7e7e4
alexgimenez02 <alex.gimenez02@estudiant.upf.edu>
2021-02-07 18:03:18 +0000

    Bueno, pues solucionado todo, se acabo por fin, nos lo hace concurrido mario

--- Lab2/code/shell.c
+++ Lab2/code/shell.c
@@ -23,29 +23,71 @@ void cmds_list(cmd_type cmds[], int* num){
         cmds[i].nargs = 0;
     }
     char ch2;
-    int fd2 = open("cmds_args.txt", O_CREAT | O_RDWR, 0644);
-    //int fd2 = open("cmds_all.txt", O_CREAT | O_RDWR, 0644);
+    //int fd2 = open("cmds.txt", O_CREAT | O_RDWR, 0644);
+    //int fd2 = open("cmds_args.txt", O_CREAT | O_RDWR, 0644);
+    int fd2 = open("cmds_all.txt", O_CREAT | O_RDWR, 0644);
     int res = read_split(fd2, cmds[ncmds].cmd, 80, &ch);
     int count = 1;
+    char tmp_char;
+    char execp[80];
     while(res > 0){
         if(ch == ' '){
             res = read_split(fd2, cmds[ncmds].args[cmds[ncmds].nargs], 80, &ch);
             cmds[ncmds].nargs++;
         }else if(ch == '\n'){
-            if(cmds[ncmds].nargs > 0){
+            if(tmp_char == '&'){
+                if(cmds[ncmds].nargs > 0){
+                    printf("%d) cmd: %s ",++j,cmds[ncmds].cmd);
+                    for(int i = 0; i < cmds[ncmds].nargs; i++){
+                        printf("argv[%d]: %s ",i+1, cmds[ncmds].args[i]);
+                    }
+                    printf("\n");
+                }else{
+                    printf("%d) cmd: %s \n",++j,cmds[ncmds].cmd);
+                }
+                tmp_char = ' ';
+            }else{
+                if(cmds[ncmds].nargs > 0){
                 printf("%d) cmd: %s ",++j,cmds[ncmds].cmd);
                 for(int i = 0; i < cmds[ncmds].nargs; i++){
-                    printf("argv[%d]: %s ",i+1, cmds[ncmds].args[i+1]);
+                    printf("argv[%d]: %s ",i+1, cmds[ncmds].args[i]);
                 }
                 printf("\n");
-            }else{
-                printf("%d) cmd: %s  %s\n",++j,cmds[ncmds].cmd, cmds[ncmds].args[cmds[ncmds].nargs]);
+                }else{
+                    printf("%d) cmd: %s  \n",++j,cmds[ncmds].cmd);
+                }
             }
             ncmds++;
             res = read_split(fd2, cmds[ncmds].cmd, 80, &ch);   
         }else if(ch == '&'){
-            ncmds++;
-            res = read_split(fd2,cmds[ncmds].cmd, 80, &ch);
+            
+            strcpy(cmds[ncmds].args[cmds[ncmds].nargs - 1],"&");
+            tmp_char = ch;
+            res = read_split(fd2,execp, 80, &ch);  
+        }else{
+            if(tmp_char == '&'){
+                if(cmds[ncmds].nargs > 0){
+                    printf("%d) cmd: %s ",++j,cmds[ncmds].cmd);
+                    for(int i = 0; i < cmds[ncmds].nargs; i++){
+                        printf("argv[%d]: %s ",i+1, cmds[ncmds].args[i]);
+                    }
+                    printf("\n");
+                }else{
+                    printf("%d) cmd: %s \n",++j,cmds[ncmds].cmd);
+                }
+                tmp_char = ' ';
+            }else{
+                if(cmds[ncmds].nargs > 0){
+                    printf("%d) cmd: %s ",++j,cmds[ncmds].cmd);
+                    for(int i = 0; i < cmds[ncmds].nargs; i++){
+                        printf("argv[%d]: %s ",i+1, cmds[ncmds].args[i]);
+                    }
+                    printf("\n");
+                }else{
+                    printf("%d) cmd: %s  \n",++j,cmds[ncmds].cmd);
+                }
+            }
+            res = 0;
         }
     }
     num = &ncmds;
@@ -59,7 +101,7 @@ void print_cmds_list(cmd_type cmds[]){
         if(cmds[ncmds].nargs > 0){
             printf("%d) cmd: %s ",++j,cmds[ncmds].cmd);
             for(int i = 0; i < cmds[ncmds].nargs; i++){
-                printf("argv[%d]: %s ",i+1, cmds[ncmds].args[i+1]);
+                printf("argv[%d]: %s ",i+1, cmds[ncmds].args[i]);
             }
             printf("\n");
         }else{
@@ -72,8 +114,8 @@ int main(int argc, char *argv[]){
     //char* cmds;
     int j = 0;
     char ch;
-    //printf("- Ejercicio 4.1\n");
-    /*
+    /*printf("- Ejercicio 4.1\n");
+    
     int fd = open("cmds.txt", O_CREAT | O_RDWR, 0644);
     while(read_split(fd, cmds, 10, &ch) > 0){
         printf("%d) %s\n", ++j, cmds);
@@ -85,6 +127,7 @@ int main(int argc, char *argv[]){
     cmds_list(cmds,&num);
     char buff[10];
     char ch_end;
+    int status;
     write(0,"- Ejercicio 4.4 & 4.5\n",sizeof("- Ejercicio 4.4 & 4.5\n"));
     while(strcmp(buff,"exit") != 0){
         write(0,"MyShell> ",strlen("MyShell> "));
@@ -93,30 +136,47 @@ int main(int argc, char *argv[]){
         if(strcmp(buff,"1") == 0 || strcmp(buff,"2") == 0 || strcmp(buff,"3") == 0 || strcmp(buff,"4") == 0 || strcmp(buff,"5") == 0 || strcmp(buff,"6") == 0 || strcmp(buff,"7") == 0 || strcmp(buff,"8") == 0 || strcmp(buff,"9") == 0){
             sscanf(buff,"%d",&char_num);
             char_num--;
-            int pid = fork();
+            int pid;
             if(strcmp(cmds[char_num].cmd,"") != 0){
                 
-                //fflush(stdout);
                 if(cmds[char_num].nargs > 0){
+                    int booleano = 0;
+                    char* cm = cmds[char_num].cmd;
+                    char* argv[cmds[char_num].nargs + 2];
+                    argv[0] = cm;
+                        
+                    for(int i = 0; i < cmds[char_num].nargs; i++){ 
+                        if(strcmp(cmds[char_num].args[i],"&") != 0){
+                            argv[i+1] = cmds[char_num].args[i];
+                        }else{
+                            booleano = 1;
+                        }
+                    }
+                    if(booleano == 1) argv[cmds[char_num].nargs] = NULL;
+                    else argv[cmds[char_num].nargs + 1] = NULL;
+                    pid = fork();
                     if(pid != 0){
                         printf("Ejecutando comando: %s ",cmds[char_num].cmd);
                         for(int i = 0; i < cmds[char_num].nargs; i++){
                             printf("%s ", cmds[char_num].args[i]);
                         } 
+                        
+                        
+                        if(booleano != 1)  wait(&status);
                     } 
-                    else if(pid == 0){                 
-                        char* cm = cmds[char_num].cmd;
-                        char* argv[cmds[char_num].nargs + 2];
-                        argv[0] = cm;
-                        for(int i = 0; i < sizeof(argv)/sizeof(*argv); i++) argv[i+1] = cmds[char_num].args[i];
-                        argv[cmds[char_num].nargs + 1] = NULL;
-                        for(int i = 0; i < cmds[char_num].nargs + 2; i++) //printf("\n%s",argv[i]);
+                    else if(pid == 0){
+
                         execvp(cm,argv);
                         _exit(1);                       
                     }
+                    
                 }else{
-                    if(pid != 0)
+                    pid = fork();
+                    if(pid != 0){
+                        fflush(stdout);
                         printf("Ejecutando comando: %s ",cmds[char_num].cmd);
+                        wait(&status);
+                    }
                     if(pid == 0){
                         char* cm = cmds[char_num].cmd;
                         char* argv[2];
@@ -141,15 +201,9 @@ int main(int argc, char *argv[]){
             write(0,"Not a valid command.\n",strlen("Not a valid command.\n"));
         }
 
-        int status;
-        wait(&status);
-        if ( WIFEXITED(status) ) {   
-            const int es = WEXITSTATUS(status);
-            if(es == 1) printf("Father: Child command found a problem\n");
-            
-        }
-    }
-    
-
-    
+        
+        
+    } 
+    while(wait(&status) > 0);
+   
 }
